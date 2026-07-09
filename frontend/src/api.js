90f53af0d23ed23db2adb64e49042f1104997066
Azeez Shaik <skazeezkhadari1234@gmail.com>
2026-07-10 00:16:25 +0530

    Fix API base URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,50 +1,34 @@
 import axios from "axios";
 
 const api = axios.create({
-
-   baseURL: "https://finreliief1.onrender.com",
-
+    baseURL: "https://finreliief2.onrender.com",
 });
 
 api.interceptors.request.use(
-
     (config) => {
-
         const token = localStorage.getItem("token");
 
         if (token) {
-
-            config.headers.Authorization =
-                `Bearer ${token}`;
-
+            config.headers.Authorization = `Bearer ${token}`;
         }
 
         return config;
-
     },
 
     (error) => Promise.reject(error)
-
 );
 
 api.interceptors.response.use(
-
     (response) => response,
 
     (error) => {
-
         if (error.response?.status === 401) {
-
             localStorage.removeItem("token");
-
             window.location.href = "/login";
-
         }
 
         return Promise.reject(error);
-
     }
-
 );
 
 export default api;
