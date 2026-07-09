df7f9ceb28d44ff74e206efaa7ba23a21ca3cce7
Azeez Shaik <skazeezkhadari1234@gmail.com>
2026-07-09 20:09:08 +0530

    Fix database engine

--- app/database.py
+++ app/database.py
@@ -11,10 +11,14 @@ DATABASE_URL = os.getenv(
     "sqlite:///./finrelief.db"
 )
 
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={"check_same_thread": False}
-)
+if DATABASE_URL.startswith("sqlite"):
+    engine = create_engine(
+        DATABASE_URL,
+        connect_args={"check_same_thread": False}
+    )
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 SessionLocal = sessionmaker(
     autocommit=False,
