84c3d27daccb4693c8fa81e021c303d2d0537bcd
Azeez Shaik <skazeezkhadari1234@gmail.com>
2026-07-09 23:30:04 +0530

    Update Vite config for Render

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173
-    }
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+  preview: {
+    host: "0.0.0.0",
+    port: process.env.PORT || 4173,
+  },
 });
